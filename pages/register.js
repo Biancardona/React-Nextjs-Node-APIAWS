a4e9f104ca06b7a8595367ec6830b73e548b7b6c
user <user@users-MacBook-Pro.local>
2022-06-20 18:34:39 -0500

    setting up RegisterPage with useState

--- pages/register.js
+++ pages/register.js
@@ -1,5 +1,73 @@
 import Layout from "../components/Layout";
+import { useState } from "react";
 
-const Register = () => <Layout>Register Page</Layout>;
+const Register = () => {
+  const [state, setState] = useState({
+    name: "",
+    email: "",
+    password: "",
+    error: "",
+    success: "",
+    buttonText: "Register",
+  });
+  const { name, email, password, error, success, buttonText } = state;
+
+  const handleChange = (name) => (e) =>
+    setState({
+      ...state,
+      [name]: e.target.value,
+      error: "",
+      success: "",
+      buttonText: "Register",
+    });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const registerForm = () => (
+    <form onSubmit={handleSubmit}>
+      <div className="form-group">
+        <input
+          value={name}
+          onChange={handleChange("name")}
+          type="text"
+          className="form-control"
+          placeholder="Type your name"
+        />
+      </div>
+      <div className="form-group">
+        <input
+          value={email}
+          onChange={handleChange("email")}
+          type="email"
+          className="form-control"
+          placeholder="Type your email"
+        />
+      </div>
+      <div className="form-group">
+        <input
+          value={password}
+          onChange={handleChange("password")}
+          type="password"
+          className="form-control"
+          placeholder="Type your password"
+        />
+      </div>
+      <div className="form-group">
+        <button className="btn btn-outline-warning">Register</button>
+      </div>
+    </form>
+  );
+  return (
+    <Layout>
+      <div className="col-md-6 offset-md-3">
+        <h1> Register </h1>
+        <br />
+        {registerForm()}
+      </div>
+    </Layout>
+  );
+};
 
 export default Register;
