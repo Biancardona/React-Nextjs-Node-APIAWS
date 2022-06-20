ccb2418e7292aa5098e8799a8113d347080e2bc3
user <user@users-MacBook-Pro.local>
2022-06-20 12:35:28 -0500

    Adding Link component, also adding .next to gitignore

--- components/Layout.js
+++ components/Layout.js
@@ -1,32 +1,40 @@
 import React from "react";
+import Head from "next/head";
+import Link from "next/link";
 
 const Layout = ({ children }) => {
+  const head = () => (
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor"
+      crossorigin="anonymous"
+    />
+  );
   const nav = () => (
-    <ul className="nav">
+    <ul className="nav nav-tabs bg-warning">
       <li className="nav-item">
-        <a className="nav-link" href="">
-          {" "}
-          Home{" "}
-        </a>
+        <Link href="/">
+          <a className="nav-link text-dark"> Home </a>
+        </Link>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="">
-          {" "}
-          Login{" "}
-        </a>
+        <Link href="/login">
+          <a className="nav-link text-dark">Login</a>
+        </Link>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="">
-          {" "}
-          Register{" "}
-        </a>
+        <Link href="/register">
+          <a className="nav-link text-dark"> Register </a>
+        </Link>
       </li>
     </ul>
   );
   return (
     <React.Fragment>
+      {head()}
       {nav()}
-      {children}
+      <div className="container pt-5 pb-5"> {children} </div>
     </React.Fragment>
   );
 };
