414a007ed53ea57e1475b7def62356fde8c0ffdb
user <user@users-MacBook-Pro.local>
2022-06-23 12:55:26 -0500

    Virtual field and userSchema methods

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const crypto = require("crypto");
 
 const userSchema = new mongoose.Schema(
   {
@@ -42,6 +43,44 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 //timestamps second argument to automatically get created add and updated ad date whenevver there is user creater on the data base.
-
 //virtual fiels
-//methods
+//usig regular function to use this keyword
+userSchema
+  .virtual("password")
+
+  .set(function (password) {
+    //create temporal variable called _password until we get the underscore password
+    this._password = password;
+    //generate salt
+    this.salt = this.makeSalt();
+    //encrypt password
+    this.hashed_password = this.encryptPassword(password);
+  })
+
+  .get(function () {
+    return this._password;
+  });
+
+//methods > authenticate, encryptPassword
+userSchema.methods = {
+  authenticate: function (plainText) {
+    return this.encryptPassword(plainText) == this.hashed_password;
+  },
+
+  encryptPassword: function (password) {
+    if (!password) return "";
+    try {
+      return crypto
+        .createHmac("sha1", "this.salt")
+        .update(password)
+        .digest("hex");
+    } catch (err) {
+      return "";
+    }
+  },
+  //When we get the password, we also need to generate salt and populated in the list schema
+  makeSalt: function () {
+    return Math.round(new Date().valueOf() * Math.random()) + "";
+  },
+};
+module.exports = mongoose.model();
